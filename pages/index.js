e0c3e2d91f840293ba273e7ecddf32b55dd99537
Freddy Calderon <freddystibencb@gmail.com>
2023-03-02 11:03:04 -0500

    Implementacion de chat Tawkto

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,20 @@ import Part3 from './pages/home/part3';
 const Home = () => {
     useEffect(()=>{
         document.body.style.overflowX = 'hidden'
+
+        let Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        let s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/6400b2f74247f20fefe394a9/1gqhbm60n';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
       },[])
+
+
+    
 
     return <>
         <Header/>
